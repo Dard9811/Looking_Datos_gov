80d9e0be22c9407154cedd60289d3e65612ab8a1
Dard9811 <da.ramirez14@uniandes.edu.co>
2019-09-18 22:51:33 -0500

    configuring backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* POST home page. */
+router.post('/data', function(req, res, next) {
+
 });
 
 module.exports = router;
